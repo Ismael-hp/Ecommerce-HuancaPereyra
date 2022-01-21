9dce1777a99a7d5e02e111ade05c8e45476815a8
mhhhhhy <hpereyra.ismael@hotmail.com>
2022-01-21 00:08:11 -0300

    Desafio entregable N|2 -- NavBar + nombre de la tienda

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import jsfundamentals from './js-fundamentals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from  "./Components/NavBar";
 
-jsfundamentals();
+//jsfundamentals();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Desafio Entregable N°1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <header className="App-header"></header>
     </div>
   );
 }
